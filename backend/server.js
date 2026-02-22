96b1cd7de44b4038a8dacb4303a09b7b3799241e
Jeevitha-reactdeveloper <bejeevitha@gmail.com>
2026-02-22 20:31:56 +0530

    fixed errors

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,8 @@ app.use(express.json({ limit: "5mb" })); // to parse req.body
 // limit shouldn't be too high to prevent DOS and its default value is 100kb
 app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
 app.use(cors({
-	origin : "http://localhost:3002",
+/* 	origin : "http://localhost:3002",*/	
+	origin: process.env.CLIENT_URL,
 	credentials : true
 }))
 app.use(cookieParser());
@@ -38,13 +39,13 @@ app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
 
-if (process.env.NODE_ENV === "production") {
+/* if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 	app.use("*", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 	});
-}
+} */
 
 /* app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
